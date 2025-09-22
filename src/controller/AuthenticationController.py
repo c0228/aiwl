9421ef2d244e8736ff26ddb2cc0ca814832b9489
Nellutla L N Rao <nlnrao228@gmail.com>
2025-09-22 18:21:09 +0100

    Updated Projects

--- src/controller/AuthenticationController.py
+++ src/controller/AuthenticationController.py
@@ -1,12 +1,51 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, request, jsonify, render_template
 from src.utils.Logger import log_message, HTTP_LOG_ID
 
 auth_bp = Blueprint("auth", __name__)
 
+# ------------------ SCREEN #1 --------------------------------------
 @auth_bp.route("/auth/init")
 def authenticate():
     log_message(HTTP_LOG_ID, "Triggered Basic Route - Hello World")
     return render_template("auth-manage-init.html")
+
+@auth_bp.route("/auth/create-account", methods=['POST'])
+def createAccount():
+    # Get data from POST request
+    data = request.get_json()  # expects JSON body
+    name = data.get('name')
+    country = data.get('country')
+    state = data.get('state')
+    mobile = data.get("mobile")
+
+    log_message(HTTP_LOG_ID, "Triggered Basic Route - Hello World")
+
+    #  Create an Account at [data/account.enc.iwl]
+
+    # Add this Account at [PHP/Apache Server]
+    #   - Credentials - pick from Environmental Variables
+
+    result = {
+        "message": f"Created Account Successfully",
+        "data": {
+            "name" : name,
+            "country" : country,
+            "state" : state,
+            "mobile": mobile
+        }
+    }
+    return jsonify(result), 200   # Return JSON with status code 200
+
+@auth_bp.route("/auth/login-account", methods=['POST'])
+def loginAccount():
+    log_message(HTTP_LOG_ID, "Triggered Basic Route - Hello World")
+    return jsonify({}), 200   # Return JSON with status code 200
+
+@auth_bp.route("/auth/export-account", methods=['POST'])
+def exportAccount():
+    log_message(HTTP_LOG_ID, "Triggered Basic Route - Hello World")
+    return jsonify({}), 200   # Return JSON with status code 200
+# -----------------------------------------------------------------
 
 @auth_bp.route("/auth/profiles/manage")
 def manageProfile():
--- src/main.py
+++ src/main.py
@@ -19,7 +19,7 @@ import os # general OS utilities (checking/removing files).
 
 from src.utils.PidFiles import stop_process
 from src.settings.constants import APP_ID, APP_NAME, APP_VERSION, PROJECT_ROOT, ROUTE_ROOT, CONTROLLER_ROOT, RPI_ROOT
-from src.utils.logger import HTTP_LOG_ID, STT_LOG_ID, LOG_FILES
+from src.utils.Logger import HTTP_LOG_ID, STT_LOG_ID, LOG_FILES
 
 # Initializes colorama so Windows & Unix terminals handle color codes.
 # autoreset=True means you don’t need to manually reset the color each time.
