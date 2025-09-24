a457bfc0fc082d854c468572a6b02b6267daa5d5
Nellutla L N Rao <nlnrao228@gmail.com>
2025-09-24 12:33:11 +0100

    Updated projects

---
+++ static/js/pages/auth-profile-create.js
@@ -0,0 +1,107 @@
+/* ------ PERSONAL DETAILS ::: START ----------------------------------------------- */
+const LOAD_CREATEPROFILE_LEFTMENUDATA = [{
+    id: "personal_details",
+    name: "Personal Details",
+    view: "createProfile_formDisplay_personalDetails"
+ },{
+    id: "edu_qualifications",
+    name: "Educational Qualifications",
+    view: "createProfile_formDisplay_eduDetails"
+ },{
+    id: "emp_details",
+    name: "Employer's Details",
+    view: "createProfile_formDisplay_empDetails"
+ },{
+    id: "visa_details",
+    name: "Visa Details",
+    view: "createProfile_formDisplay_visaDetails"
+ },{
+    id: "tech_stack_details",
+    name: "Tech Stack Details",
+    view: "createProfile_formDisplay_techStackDetails"
+ }];
+function sel_createProfile_leftMenu(activeId){
+ for(let i=0;i<LOAD_CREATEPROFILE_LEFTMENUDATA?.length;i++){
+    const menuId = LOAD_CREATEPROFILE_LEFTMENUDATA[i]?.id;
+    const view = LOAD_CREATEPROFILE_LEFTMENUDATA[i]?.view;
+    if(activeId===menuId){
+        $('#'+menuId).removeClass('disabled');
+        $('#'+menuId).addClass('active fWBold');
+        document.getElementById(view).style.display='block';
+    } else {
+        $('#'+menuId).removeClass('active fWBold');
+        $('#'+menuId).addClass('disabled');
+        document.getElementById(view).style.display='none';
+    }
+ }
+}
+function load_createProfile_leftMenu(id, activeId) {
+ let menuView;
+ let content='<ul class="nav nav-pills flex-column">';
+    for(let i=0;i<LOAD_CREATEPROFILE_LEFTMENUDATA?.length;i++){
+        const menuId = LOAD_CREATEPROFILE_LEFTMENUDATA[i]?.id;
+        const menuName = LOAD_CREATEPROFILE_LEFTMENUDATA[i]?.name;
+        content+='<li class="nav-item">';
+        if(activeId===menuId) {
+            menuView = LOAD_CREATEPROFILE_LEFTMENUDATA[i]?.view;
+            content+='<a id="'+menuId+'" class="nav-link active fWBold" href="#">'+menuName+'</a>';
+        } else {
+            content+='<a id="'+menuId+'" class="nav-link disabled" href="#">'+menuName+'</a>';
+        }
+        content+='</li>';
+    }
+    content+='</ul>';
+ document.getElementById(id).innerHTML = content;
+ document.getElementById(menuView).style.display='block';
+}
+function save_createProfile_personalDetails(){
+
+}
+function next_createProfile_personalDetails(){
+ sel_createProfile_leftMenu('edu_qualifications');
+}
+/* ------ PERSONAL DETAILS ::: END ----------------------------------------------- */
+/* ------ EDUCATIONAL QUALIFICATIONS ::: START ----------------------------------- */
+function previous_createProfile_eduDetails(){
+ sel_createProfile_leftMenu('personal_details');
+}
+function save_createProfile_eduDetails(){
+ 
+}
+function next_createProfile_eduDetails(){
+ sel_createProfile_leftMenu('emp_details');
+}
+/* ------ EDUCATIONAL QUALIFICATIONS ::: END ----------------------------------- */
+/* ------ EMPLOYEE'S DETAILS ::: START ----------------------------------- */
+function previous_createProfile_empDetails(){
+ sel_createProfile_leftMenu('edu_qualifications');
+}
+function save_createProfile_empDetails(){
+
+}
+function next_createProfile_empDetails(){
+ sel_createProfile_leftMenu('visa_details');
+}
+/* ------ EMPLOYEE'S DETAILS ::: END ----------------------------------- */
+/* ------ VISA DETAILS ::: START ----------------------------------- */
+function previous_createProfile_visaDetails(){
+ sel_createProfile_leftMenu('emp_details');
+}
+function save_createProfile_visaDetails(){
+
+}
+function next_createProfile_visaDetails(){
+ sel_createProfile_leftMenu('tech_stack_details');
+}
+/* ------ VISA DETAILS ::: END ----------------------------------- */
+/* ------ TECH STACK DETAILS ::: START ----------------------------------- */
+function previous_createProfile_techStackDetails(){
+ sel_createProfile_leftMenu('visa_details');
+}
+function save_createProfile_techStackDetails(){
+
+}
+function publish_createProfile_techStackDetails(){
+ 
+}
+/* ------ TECH STACK DETAILS ::: END ----------------------------------- */
